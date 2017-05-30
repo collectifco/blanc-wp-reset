0d03ca94ed2f15cd2e3fa72e5f2e3a6eaa6acb1d
josiahwiebe <josiah@collectif.co>
2017-05-30 16:18:12 -0500

    remove bourbon/neat, add flex helpers, update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,31 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var notify = require('gulp-notify');
-var sass = require('gulp-sass');
-var cssmin = require('gulp-cssmin');
-var watch = require('gulp-watch');
-var neat = require('node-neat').includePaths;
+var gulp = require('gulp')
+var autoprefixer = require('gulp-autoprefixer')
+var notify = require('gulp-notify')
+var sass = require('gulp-sass')
+var cssmin = require('gulp-cssmin')
+var babel = require('gulp-babel')
+var concat = require('gulp-concat')
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
 
 var paths = {
   scripts: 'js/',
   scss: '_assets/scss/**/*.scss',
-  css: '_assets/css'
-};
+  css: '_assets/css',
+  js: '_assets/js/**/*.js'
+}
 
 gulp.task('watch', ['sass'], function() {
-  gulp.watch(paths.scss, ['sass']);
-});
+  gulp.watch(paths.scss, ['sass'])
+  gulp.watch(paths.js, ['js'])
+})
 
 gulp.task('sass', function() {
   gulp.src('_assets/scss/*.scss')
-    .pipe(sass({
-      includePaths: ['sass'].concat(neat)
-    }))
+    .pipe(sass())
     .on('error', function(err) {
-      console.log(err.message);
-      return notify().write(err); // send the error through Growl
+      console.log(err.message)
+      return notify().write(err)
     })
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
@@ -31,7 +33,20 @@ gulp.task('sass', function() {
     }))
     .pipe(cssmin())
     .pipe(gulp.dest('.'))
-    .pipe(notify('Sass compiled successfully.'));
-});
+    .pipe(notify('Sass compiled successfully.'))
+})
 
-gulp.task('default', ['watch','sass']);
+gulp.task('js', function() {
+  gulp.src(paths.js)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(concat('index.js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(notify('Minified JS.'))
+})
+
+
+gulp.task('default', ['watch','sass', 'js'])
